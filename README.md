1905cbc97b1c3cdc42c17594440e9f0d34181002
root <root@evnpinfsanl01.tceh.net>
2019-02-02 17:55:12 -0600

    commit 2

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # snapshot_analyzer
-AWS Snapshot
+ Acloutd Guru - Python for Beginers project
+
+## About
+
+This is a script for listing and creating EC2 Instances snapshots
+
+### Configuring ###
+
+Uses AWS CLI
+
+## Running ##
+
+/home/shazari/scripts/aws/code/snapshot_analyzer/snapshot/snapshot.py
+
+
--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3
 import boto3
-session = boto3.Session(profile_name='default',region_name='us-east-1')
-ec2 = session.resource('ec2')
-for i in ec2.instances.all():
-    print(i)
+
+if __name__ == "__main__":
+    session = boto3.Session(profile_name='default',region_name='us-east-1')
+    ec2 = session.resource('ec2')
+    for i in ec2.instances.all():
+        print(i)
 
 #s3  = session.resource('s3')
 #for i in s3.buckets.all():
